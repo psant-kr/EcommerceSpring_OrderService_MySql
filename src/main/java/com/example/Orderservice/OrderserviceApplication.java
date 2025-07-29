0e08f91fbb1f495586ee923902695952476724f0
prashant kumar <prashantdoa1996@gmail.com>
2025-07-29 13:41:22 +0530

    ✨ Db integration and microservice setup done
    git push

---
+++ src/main/java/com/example/Orderservice/OrderserviceApplication.java
@@ -0,0 +1,18 @@
+package com.example.Orderservice;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class OrderserviceApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load(); // Load environment variables from .env file
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue())); // Set system properties from environment variables and set them
+		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+
+}
