04ba08ddd1426e517c71e302bc3e18e48a9134e0
prashant kumar <prashantdoa1996@gmail.com>
2025-08-02 17:04:07 +0530

    ✨ Eureka client and resttemplate added to call product service
    done

--- src/main/java/com/example/Orderservice/clients/ProductServiceClient.java
+++ src/main/java/com/example/Orderservice/clients/ProductServiceClient.java
@@ -1,7 +1,6 @@
 package com.example.Orderservice.clients;
 
 import com.example.Orderservice.dto.ProductDTO;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,17 +8,15 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class ProductServiceClient {
 
-    private final RestTemplateBuilder restTemplateBuilder;
+    private final RestTemplate restTemplate;
 
-    public ProductServiceClient(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplateBuilder = restTemplateBuilder;
+    public ProductServiceClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public ProductDTO getProductById(Long productId){
-
-        RestTemplate restTemplate = restTemplateBuilder.build();
         // not the best way just use eureka
-        String url = "http://localhost:3000/api/products/" + productId;
+        String url = "http://ECOMMERCESPRING/api/products/" + productId;
 
         ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url, ProductDTO.class);
         return response.getBody();
